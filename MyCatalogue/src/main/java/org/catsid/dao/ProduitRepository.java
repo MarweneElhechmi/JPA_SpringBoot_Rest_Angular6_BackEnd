dcd59cce961c369bdb93baeb80737260f25a9dd3
marwe <marwe@DESKTOP-H3KS6MS>
2017-12-24 11:55:00 +0100

    Repo on a ajouté des fonctions et des Query pour les utiliser aprés

--- MyCatalogue/src/main/java/org/catsid/dao/ProduitRepository.java
+++ MyCatalogue/src/main/java/org/catsid/dao/ProduitRepository.java
@@ -1,8 +1,29 @@
 package org.catsid.dao;
 
+import java.util.List;
+
 import org.catsid.entities.Produit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProduitRepository extends JpaRepository<Produit, Long>{
 
+	@Query("select p from Produit p where p.designation :x")
+	public Page<Produit> ProduitParMotCle(String motCle,Pageable page);
+	
+	// Appelation findBy'Nom Variable avec premiére lettre majuscule'
+	// Ici il va faire une requêtte : 
+	// Select * from Produit p where p.designation= :x ;
+	// Or on veux : Select * from Produit p where p.designation like :x ;
+	// Donc on fait une fonction : ProduitParMotCle
+	public List<Produit> findByDesignation(String designation);
+	
+	// Toujours dans Spring si on fait une fonction findBy'Nom variable' exemple : 
+	// findByIdVariableDansEntities(Long idParamétre); c'est-à-dire que Spring va exécuter la requêtte : 
+	// Select * from Produit p where p.idVariableDansEntities= :idParamétre ;
+	// Normalement findById c'est tout mais j'ai ajouté dans l'explication
+	// findByIdVariableDansEntities pour mieux comprendre
+	public Page<Produit> findByDesignation(String designation,Pageable page);
 }
