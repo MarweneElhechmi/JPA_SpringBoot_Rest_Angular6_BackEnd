5ec697be6806cc2fbf2a8c4adf3a33b91ee08906
marwe <marwe@DESKTOP-H3KS6MS>
2017-12-24 12:05:31 +0100

    On a ajouté @Param("x") pour comprendre que le mc est le x de la requette

--- MyCatalogue/src/main/java/org/catsid/dao/ProduitRepository.java
+++ MyCatalogue/src/main/java/org/catsid/dao/ProduitRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProduitRepository extends JpaRepository<Produit, Long>{
 
 	@Query("select p from Produit p where p.designation :x")
-	public Page<Produit> ProduitParMotCle(String motCle,Pageable page);
+	public Page<Produit> ProduitParMotCle(@Param("x") String motCle,Pageable page);
 	
 	// Appelation findBy'Nom Variable avec premiére lettre majuscule'
 	// Ici il va faire une requêtte : 
