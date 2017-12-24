1ee9ef33ff9d5331b1cd598efe96765f1cd9e065
marwe <marwe@DESKTOP-H3KS6MS>
2017-12-24 11:52:10 +0100

    Controller ou service qui aide à exécuter les CRUD

--- MyCatalogue/src/main/java/org/catsid/controllers/CatalogueController.java
+++ MyCatalogue/src/main/java/org/catsid/controllers/CatalogueController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.catsid.dao.ProduitRepository;
 import org.catsid.entities.Produit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,11 @@ public class CatalogueController {
 	@RequestMapping(value="/all")
 	public List<Produit> getProduit() {
 		return produitRepository.findAll();
+	}
+	
+	@RequestMapping(value="/produits")
+	public Page<Produit> getProduit(int page) {
+		return produitRepository.findAll(new PageRequest(page, 5));
+		
 	}
 }
